5e553d2f2292483c8889002ce097f347e47a42d5
kareena0211 <kareena22@navgurukul.org>
2024-05-09 23:10:48 +0530

    stopwatch code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./zuvy projects/Navbar";
-import Tic_Tac_Toe_Game from "./zuvy projects/Tic_Tac_Toe_Game";
+// import Tic_Tac_Toe_Game from "./zuvy projects/Tic_Tac_Toe_Game";
+import Stopwatch from "./zuvy projects/Stopwatch";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Tic_Tac_Toe_Game />
+    {/*  <Tic_Tac_Toe_Game />*/}
+      <Stopwatch />
     </>
   );
 };
